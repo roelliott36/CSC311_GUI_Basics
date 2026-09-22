94514eea9a0e1373fe163dd41fbd16b999fd2a22
Rochelle Elliott <ro.elliott36@gmail.com>
2026-09-22 10:31:57 -0400

    added a styles.css file to later edit and a new view to display the main page after button click

---
+++ src/main/java/org/example/csc311_gui_basics/HelloController.java
@@ -0,0 +1,15 @@
+package org.example.csc311_gui_basics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("My name is Rochelle Elliott!");
+
+    }
+}
---
+++ src/main/java/org/example/csc311_gui_basics/mainPageController.java
@@ -0,0 +1,4 @@
+package org.example.csc311_gui_basics;
+
+public class mainPageController {
+}
