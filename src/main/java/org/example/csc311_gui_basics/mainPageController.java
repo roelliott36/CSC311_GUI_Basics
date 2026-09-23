fed019f34b3e37c9b505b1ec420ad67144be0711
Rochelle Elliott <ro.elliott36@gmail.com>
2026-09-22 22:40:01 -0400

    set up the controller so basic aspects are functional

--- src/main/java/org/example/csc311_gui_basics/mainPageController.java
+++ src/main/java/org/example/csc311_gui_basics/mainPageController.java
@@ -1,4 +1,110 @@
 package org.example.csc311_gui_basics;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class mainPageController {
+
+    @FXML
+    private ListView<String> educationListView;
+
+    @FXML
+    private ToggleButton educationSwitchButton;
+
+    @FXML
+    private ImageView educationImageView;
+
+    @FXML
+    private Button educationPreviousButton;
+
+    @FXML
+    private Button educationNextButton;
+
+    @FXML
+    private ListView<String> currentPositionListView;
+
+    @FXML
+    private ListView<String> previousPositionListView;
+
+    @FXML
+    private ListView<String> previousJobListView;
+
+    private boolean showingCollege = true;
+
+    private final String[] collegeInfo = {
+            "Farmingdale State College",
+            "Major: Computer Programming and Information Systems",
+            "Minor: Business Analytics",
+            "Goal: Data Analytics Certification"
+    };
+
+    private final String[] highSchoolInfo = {
+            "Central Islip High School",
+            "Graduated in the Top 10% of my class",
+            "Honor Roll",
+            "Honors Society",
+            "President, Science Technology Entry Program (STEP) Club",
+            "STEP Club sponsored by NYIT",
+            "Computer Science Ambassador for Young Women - Code.org",
+            "Long Island's Extraordinary Seniors - Newsday"
+    };
+
+    @FXML
+    public void initialize() {
+
+        loadCollegeInfo();
+
+        currentPositionListView.getItems().addAll(
+                "IT Help Desk Analyst",
+                "Zwanger Pesiri Radiology"
+        );
+
+        previousPositionListView.getItems().addAll(
+                "Scheduler",
+                "Zwanger Pesiri Radiology",
+                "Duration: 1 year"
+        );
+
+        previousJobListView.getItems().addAll(
+                "Team Member",
+                "Chick-fil-A",
+                "Duration: 2 years"
+        );
+
+        educationSwitchButton.setOnAction(event -> switchEducation());
+
+        educationPreviousButton.setOnAction(event -> previousEducationImage());
+
+        educationNextButton.setOnAction(event -> nextEducationImage());
+    }
+
+    private void loadCollegeInfo() {
+        educationListView.getItems().setAll(collegeInfo);
+        educationSwitchButton.setText("High School");
+        showingCollege = true;
+    }
+
+    private void loadHighSchoolInfo() {
+        educationListView.getItems().setAll(highSchoolInfo);
+        educationSwitchButton.setText("College");
+        showingCollege = false;
+    }
+
+    private void switchEducation() {
+        if (showingCollege) {
+            loadHighSchoolInfo();
+        } else {
+            loadCollegeInfo();
+        }
+    }
+
+    private void previousEducationImage() {
+        // Education image navigation will be added when images are provided.
+    }
+
+    private void nextEducationImage() {
+        // Education image navigation will be added when images are provided.
+    }
 }
